adc2f81f7a992f46116a3ba62da7e36d907d0304
SlubblesThuggy <65078309+SlubblesThuggy@users.noreply.github.com>
2024-02-06 20:42:26 +0100

    Implemented getting the computer's choice
    Added func: getComputerChoice(), that returns randomly either rock,
    paper or scissors.

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log("JavaScript is working.");
+function getComputerChoice() {
+    let choice = Math.floor(Math.random() * 3);
+
+    switch (choice) {
+        case 0:
+            return "rock";
+
+        case 1:
+            return "paper";
+
+        case 2:
+            return "scissors";
+    }
+}
+
+console.log(getComputerChoice());
