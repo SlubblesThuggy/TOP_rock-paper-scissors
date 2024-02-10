d9ff931a3d33e8395d32de7a9781db0d50248ac9
SlubblesThuggy <65078309+SlubblesThuggy@users.noreply.github.com>
2024-02-10 11:51:49 +0100

    Added playGame function
    Calls the playRound function in an infinite loop with player input (prompt),
    and exits when the user presses cancel.

--- script.js
+++ script.js
@@ -24,22 +24,21 @@ function playRound(playerSelection, computerSelection) {
      || playerSelection === "paper" && computerSelection === "rock"
      || playerSelection === "scissors" && computerSelection === "paper"
     ) {
-        return "Player won.";
+        return `You win. ${playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1)} beats ${computerSelection}.`;
     } else {
-        return "Computer won.";
+        return `You lose. ${computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1)} beats ${playerSelection}.`;
     }
 }
 
-let playerSelection = prompt("Make a choice.");
-let computerSelection = getComputerChoice();
+function playGame() {
+    while (true) {
+        let playerSelection = prompt("Make a choice.");
+        if (playerSelection == null) break;
 
-let roundResult = playRound(playerSelection, computerSelection);
+        let computerSelection = getComputerChoice();
+        let roundResult = playRound(playerSelection, computerSelection);
+        console.log(roundResult);
+    }
+}
 
-console.log(
-`ROUND RESULT
-
-player choice: ${playerSelection}
-computer choice: ${computerSelection}
-
-${roundResult}`
-);
+playGame();
